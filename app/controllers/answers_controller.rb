7031a15a47dfab0c7554e0dd1baca0a33a0f52a8
djeiky <djeik@inbox.ru>
2017-06-19 20:53:00 +0300

    add answers controller specs

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,10 +1,24 @@
 class AnswersController < ApplicationController
+  before_action :set_question, only: [:new, :create]
+
+  def new
+    @answer = @question.answers.build
+  end
+
   def create
-    @answer = Answer.new(answer_params)
-    @answer.save
+    @answer = @question.answers.new(answer_params)
+    if @answer.save
+      redirect_to question_path(@question)
+    else
+      render :new
+    end
   end
 
   private
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
 
   def answer_params
     params.require(:answer).permit(:body, :question_id)
---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe AnswersController, :type => :controller do
+
+  let(:answer) {create :answer}
+  let(:question) { create :question }
+
+  describe 'GET #new' do
+    before { get :new, params: {question_id: question}}
+
+    it 'assigns @answer to a new Answer, that has Question' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'render new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'valid answer' do
+      it 'save answer to database' do
+        expect{post :create, params: {question_id: question, answer: attributes_for(:answer)}}.to change(question.answers, :count).by(1)
+      end
+
+      it 'redirect to question show view' do
+        post :create, params: {question_id: question, answer: attributes_for(:answer)}
+        expect(response).to redirect_to question_path(question)
+      end
+
+
+    end
+
+    context 'invalid answer' do
+      it 'not save to database' do
+        expect{post :create, params: {question_id: question, answer: attributes_for(:invalid_answer)}}.to_not change(question.answers, :count)
+      end
+
+      it 'render new view' do
+        post :create, params: {question_id: question, answer: attributes_for(:invalid_answer)}
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+
+
+
+
+  describe 'POST #create' do
+  end
+
+end
+
